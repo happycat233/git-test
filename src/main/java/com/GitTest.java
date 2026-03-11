ea5f82f7b9bd6807337e80c9e08627ae32acbd40
happycat233 <wuhao20060815@163.com>
2026-03-11 15:25:47 +0800

    master commit 2

--- src/main/java/com/GitTest.java
+++ src/main/java/com/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("Hello World4");
         System.out.println("master test");
         System.out.println("hot-fix 1");
+        System.out.println("hot-fix 2");
     }
 }
